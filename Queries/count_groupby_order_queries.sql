2d4adde44c5571356f52288e728e8fb9c13608f1
Chloe Ittel <chloeittel@Chloes-Air.woh.rr.com>
2021-09-26 16:15:10 -0400

    Queries folder

---
+++ Queries/count_groupby_order_queries.sql
@@ -0,0 +1,25 @@
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number and ordered by dept_no
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Save the employee count by department number and ordered by dept_no as a table "dept_no_count" 
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_no_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM dept_no_count;
