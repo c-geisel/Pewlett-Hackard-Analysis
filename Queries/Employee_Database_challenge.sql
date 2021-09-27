6275407df9d19a479e09220da39ccea4afd49c54
Chloe Ittel <chloeittel@Chloes-Air.woh.rr.com>
2021-09-27 16:23:53 -0400

    completed challenge queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,19 +34,21 @@ ORDER BY COUNT (ut.title) DESC;
 SELECT*FROM retiring_titles;
 
 --Deliverable 2
---Deliverable 2
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.birth_date
-	de.from_date,
-	de.to_date,
-	ti.title
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
 INTO mentorship_eligibility
-FROM employees AS e
-	INNER JOIN dept_emp AS de
-		ON e.emp_no = de.emp_no
-	INNER JOIN titles AS ti
-		ON e.emp_no = ti.emp_no
+FROM employees as e
+    INNER JOIN dept_emp as de
+    	ON (e.emp_no = de.emp_no)
+    INNER JOIN titles as ti
+    	ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
 
---not done with code, left off  on step 8
+SELECT*FROM mentorship_eligibility;
